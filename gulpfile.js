2aea609e2dc7d39aacf75ba43947fa9470042d43
unknown <aespiritu@01013WS003.domain.gdpsupergroup.com>
2016-05-24 14:09:17 -0500

    ES6 to ES5 transpile

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,36 @@
 
 // Packages
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 
 
 // Tasks
+
+// Uglify
 gulp.task('games-js', function(){
 
-    gulp
+    return gulp
         .src('resources/assets/js/games/index.js')
         .pipe(uglify())
         .pipe(gulp.dest('public/js/games'));
+
+});
+
+
+// Browserify & Babel
+gulp.task('users-js', function(){
+
+    return browserify('resources/assets/js/users/index.js')
+        .transform('babelify', {presets: ['es2015']})
+        .bundle()
+        .pipe(source('user-bundle.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js/users'));
 
 });
